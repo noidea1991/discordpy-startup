44e6deec78a97fc2726109657ab32efa1498fccd
noidea1991 <48171107+noidea1991@users.noreply.github.com>
2020-06-16 19:55:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def group(ctx, specified_num=1):
 @bot.command()
 async def list():
     make_list = MakeTeam()
-    msg = self.channel_mem
+    msg = make_team.set_mem(self, ctx)
     await ctx.channel.send(msg)
     
 """botの接続と起動"""
